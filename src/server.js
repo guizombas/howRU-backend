b50511a333b611ae216eaad96ddb1900a3c0738e
Guizombas <guilherme.gabriel.532@gmail.com>
2021-02-19 01:52:00 -0300

    mudanca de ip para cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new socketIO(server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: ["http://192.168.0.108:3000", "http://localhost:3000"],
         methods: ["GET", "POST"]
     }
 })
